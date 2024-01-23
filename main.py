901ccda21c7a2f66a3e785a2df76056ff1974f9b
Gabriel Bac <gabibac8@gmail.com>
2024-01-23 17:30:53 +0000

    Load image func defined. Config params changed for grayscale image.

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ import mrcnn.visualize as visualize
 
 
 # Current working directory of the project
-ROOT_DIR = os.getcwd()
+ROOT_DIR = os.path.abspath("")
 # Local path to trained weights file
 COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 # Path to save logs and trained model
 DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+# Path to dataset
+DATASET_DIR = "raws"
+
+#Counts ROUGHS
+TRAIN_IMAGE_COUNT = 660
+VALIDATION_IMAGE_COUNT = 250
 
 ############################################################
 #  Configurations
@@ -30,7 +36,9 @@ class VesselConfig(Config):
 
     NAME = "vessels"
     GPU_COUNT = 1
-    IMAGES_PER_GPU = 2
+    IMAGES_PER_GPU = 1
+    IMAGE_CHANNEL_COUNT = 1
+    MEAN_PIXEL = np.array([128])
     NUM_CLASSES = 1 + 1 # background + vessel(s)
     IMAGE_MIN_DIM = 256
     IMAGE_MAX_DIM = 256
@@ -45,22 +53,23 @@ class VesselConfig(Config):
 ############################################################
 
 class VesselDataset(utils.Dataset):
-    """Imports US dataset
+    """_summary_
 
     Args:
         utils (_type_): _description_
     """
     def load_images(self, dataset_dir, is_train: bool):
-        """_summary_
+        """Load training and validation images from 
 
         Args:
-            dataset_dir (os.path): e.g. "raws"
+            dataset_dir (_type_): _description_
+            is_train (bool): _description_
         """
         # Add class(es)
         self.add_class("vessels", 1, "vessel")
 
         #Get image IDs from directory names
-        image_ids = next(os.walk(os.path.join(os.getcwd(), dataset_dir)))[2]
+        image_ids = next(os.walk(os.path.join(ROOT_DIR, dataset_dir)))[2]
 
         #Add images
         for idx, image_id in enumerate(image_ids):
@@ -69,20 +78,19 @@ class VesselDataset(utils.Dataset):
             if not is_train and idx < int(len(image_ids)*0.75):
                 continue
             self.add_image(
-                "vessels",
+                source="vessels",
                 image_id=image_id,
                 path=os.path.join(dataset_dir, image_id)
             )
     
     def load_mask(self, image_id: int):
-        """Returns the mask of a specific image
+        """Returns mask(s) and class_id for each mask.
 
         Args:
-            image_id (int): Image ID
+            image_id (int): _description_
 
         Returns:
-            mask: _description_
-            class_id_array: _description_
+            _type_: _description_
         """
         #Get image info
         info = self.image_info[image_id]
@@ -98,21 +106,45 @@ class VesselDataset(utils.Dataset):
         #Return mask and the corresponding class ID array
         return mask.astype(bool), class_id_array
 
+    def image_reference(self, image_id):
+        """Return path of the image.
+
+        Args:
+            image_id (int): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        info = self.image_info[image_id]
+        return info["path"]
+    
+    def load_image(self, image_id):
+        """Load the specified image and return a [H,W,1] Numpy array.
+        """
+        # Load image
+        image = cv2.imread(self.image_info[image_id]['path'], cv2.IMREAD_GRAYSCALE)
+        image = image[..., np.newaxis]
+        return image
 ############################################################
 #  Run
 ############################################################
 
 config = VesselConfig()
+
+#Training dataset
 dataset_train = VesselDataset()
-dataset_train.load_images("raws", is_train=True)
+dataset_train.load_images(DATASET_DIR, is_train=True)
 dataset_train.prepare()
+#Validation dataset
 dataset_val = VesselDataset()
-dataset_val.load_images("raws", is_train=False)
+dataset_val.load_images(DATASET_DIR, is_train=False)
 dataset_val.prepare()
 
-model = modellib.MaskRCNN(mode="training", config=config, model_dir=DEFAULT_LOGS_DIR) #Create MaskRCNN model object
-model.load_weights("mask_rcnn_coco.h5", by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc","mrcnn_bbox", "mrcnn_mask"]) #Load COCO weights
+#Create MaskRCNN model object
+model = modellib.MaskRCNN(mode="training", config=config, model_dir=DEFAULT_LOGS_DIR)
+#Load COCO weights
+model.load_weights(COCO_MODEL_PATH, by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc","mrcnn_bbox", "mrcnn_mask", "conv1"])
 
 #Train
-model.train(dataset_train, dataset_val, learning_rate=config.LEARNING_RATE, epochs=20, layers='heads') #Train only head layers
-# model.train(dataset_train, dataset_val, learning_rate=config.LEARNING_RATE, epochs=90, layers='all') #Train all layers
+# model.train(dataset_train, dataset_val, learning_rate=config.LEARNING_RATE, epochs=15, layers='heads') #Train only head layers
+model.train(dataset_train, dataset_val, learning_rate=config.LEARNING_RATE, epochs=15, layers='all') #Train all layers
